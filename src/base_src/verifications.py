b5e143080355431a78d0a7090cef3610c929d2b7
JolonB <jolon.behrent@gmail.com>
2020-10-29 14:44:33 +1300

    convert verifications into function

--- src/base_src/verifications.py
+++ src/base_src/verifications.py
@@ -1,4 +1,5 @@
 # Verify config files
+# Should be used when first booting up (and maybe periodically while running) to ensure none of the config files are invalid
 
 import config_const
 import config_example as config_secret
@@ -17,46 +18,50 @@ def contains_keys(name: str, d: dict, *keys: tuple):
             )
 
 
-# `email` checks
-contains_keys(
-    "email", config_secret.email, ("address", str), ("password", str), ("port", int)
-)
-assert check_addr(config_secret.email["address"]), "address in `email` is not valid"
-
-
-# `email_recipients` checks
-for addr, key in config_secret.email_recipients.items():
-    assert check_addr(addr), "address {} in `email_recipients` is not valid".format(
-        addr
-    )
-    assert isinstance(key, int), "key {} in `email_recipients` is not integer".format(
-        key
-    )
-
-# `mobile_recipients` checks
-for phone, key in config_secret.mobile_recipients.items():
-    assert isinstance(
-        phone, str
-    ), "phone number {} in `mobile_recipients` is not string".format(phone)
-    assert isinstance(key, int), "key {} in `mobile_recipients` is not integer".format(
-        key
-    )
-
-# `user_info` checks
-for key, values in config_secret.user_info.items():
-    assert isinstance(key, int), "key {} in `user_info` is not integer".format(key)
+def verify():
+    # `email` checks
     contains_keys(
-        "user_info[{}]".format(key), values, ("rank", int), ("pin", str), ("salt", str)
+        "email", config_secret.email, ("address", str), ("password", str), ("port", int)
     )
-    assert (
-        values["rank"] in config_const.ranks.values()
-    ), "rank {} in {} is not in config_const.ranks".format(values["rank"], values)
+    assert check_addr(config_secret.email["address"]), "address in `email` is not valid"
 
-# `two_fact_auth` checks
-assert isinstance(config_secret.two_fact_auth, bool)
+    # `email_recipients` checks
+    for addr, key in config_secret.email_recipients.items():
+        assert check_addr(addr), "address {} in `email_recipients` is not valid".format(
+            addr
+        )
+        assert isinstance(
+            key, int
+        ), "key {} in `email_recipients` is not integer".format(key)
 
-# `notif_kw` checks
-for key, value in config_secret.notif_kw.items():
-    assert (
-        value in config_const.keywords.values()
-    ), "keyword {} in notif_kw is not in config_const.ranks".format(value)
+    # `mobile_recipients` checks
+    for phone, key in config_secret.mobile_recipients.items():
+        assert isinstance(
+            phone, str
+        ), "phone number {} in `mobile_recipients` is not string".format(phone)
+        assert isinstance(
+            key, int
+        ), "key {} in `mobile_recipients` is not integer".format(key)
+
+    # `user_info` checks
+    for key, values in config_secret.user_info.items():
+        assert isinstance(key, int), "key {} in `user_info` is not integer".format(key)
+        contains_keys(
+            "user_info[{}]".format(key),
+            values,
+            ("rank", int),
+            ("pin", str),
+            ("salt", str),
+        )
+        assert (
+            values["rank"] in config_const.ranks.values()
+        ), "rank {} in {} is not in config_const.ranks".format(values["rank"], values)
+
+    # `two_fact_auth` checks
+    assert isinstance(config_secret.two_fact_auth, bool)
+
+    # `notif_kw` checks
+    for key, value in config_secret.notif_kw.items():
+        assert (
+            value in config_const.keywords.values()
+        ), "keyword {} in notif_kw is not in config_const.ranks".format(value)
