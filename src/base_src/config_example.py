e497c9396e201a5352c3ed1f15b71e7d5ac46ad8
JolonB <jolon.behrent@gmail.com>
2020-10-29 14:38:35 +1300

    update configuration files

---
+++ src/base_src/config_example.py
@@ -0,0 +1,29 @@
+# The address to send notifications from
+
+from config_const import *
+
+# Credentials for email sending service
+email = {
+    "address": "name@gmail.com",
+    "password": "password",
+    "port": 465, # this can normally be left at 465
+}
+
+# The addresses to send notifications to and their security codes
+email_recipients = {
+    "name1@address1.com": 1,
+    "name2@address2.com": 2,
+}
+# The mobile numbers to send notifications to
+mobile_recipients = {
+    "number1": 1,
+    "number2": 2,
+}
+
+# The information about registered users. The key maps to values in email_/mobile_recipients
+user_info = {1:{"rank": ranks["ADMIN"], "pin": "1234", "salt": "put some salt here"}, 2:{"rank": ranks["USER"], "pin": "5678", "salt": "put different salt here"}}
+# Two factor authentication. Set to True if enabled
+two_fact_auth = True
+
+# Notification keywords. Change the keys if you want to provide some security through obscurity
+notif_kw = {"DISABLE": keywords["DISABLE"], "ENABLE": keywords["ENABLE"], "MUTE": keywords["MUTE"]}
---
+++ src/base_src/verifications.py
@@ -0,0 +1,41 @@
+# Verify config files
+
+import config_const
+import config_example as config_secret
+
+def check_addr(email_addr: str) -> bool:
+    return '@' in email_addr
+
+def contains_keys(name:str, d:dict, *keys: tuple):
+    for value, type_ in keys:
+        assert value in d, "missing {} in `{}`".format(value,name)
+        if type_:
+            assert isinstance(d[value], type_), "{} in {} is not {}".format(d[value], name, type_)
+
+# `email` checks
+contains_keys("email", config_secret.email, ("address",str), ("password",str), ("port",int))
+assert check_addr(config_secret.email["address"]), "address in `email` is not valid"
+
+
+# `email_recipients` checks
+for addr, key in config_secret.email_recipients.items():
+    assert check_addr(addr), "address {} in `email_recipients` is not valid".format(addr)
+    assert isinstance(key, int), "key {} in `email_recipients` is not integer".format(key)
+
+# `mobile_recipients` checks
+for phone, key in config_secret.mobile_recipients.items():
+    assert isinstance(phone, str), "phone number {} in `mobile_recipients` is not string".format(phone)
+    assert isinstance(key, int), "key {} in `mobile_recipients` is not integer".format(key)
+
+# `user_info` checks
+for key, values in config_secret.user_info.items():
+    assert isinstance(key, int), "key {} in `user_info` is not integer".format(key)
+    contains_keys("user_info[{}]".format(key), values, ("rank", int), ("pin", str), ("salt",str))
+    assert values["rank"] in config_const.ranks.values(), "rank {} in {} is not in config_const.ranks".format(values["rank"], values)
+
+# `two_fact_auth` checks
+assert isinstance(config_secret.two_fact_auth, bool)
+
+# `notif_kw` checks
+for key, value in config_secret.notif_kw.items():
+    assert value in config_const.keywords.values(), "keyword {} in notif_kw is not in config_const.ranks".format(value)
