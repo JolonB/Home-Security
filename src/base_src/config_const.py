52b51a6f3c46eee5813fe57a365af940e1ca7f40
JolonB <jolon.behrent@gmail.com>
2020-10-29 14:41:20 +1300

    fix linting

--- src/base_src/config_const.py
+++ src/base_src/config_const.py
@@ -1,14 +1,14 @@
 # Ranks
 ranks = {
-"ADMIN":0,
-"MAINTAINER":1,
-"USER":2,
-"GUEST":3,
+    "ADMIN": 0,
+    "MAINTAINER": 1,
+    "USER": 2,
+    "GUEST": 3,
 }
 
 # Keywords
 keywords = {
-"DISABLE":0,
-"ENABLE":1,
-"MUTE":2,
+    "DISABLE": 0,
+    "ENABLE": 1,
+    "MUTE": 2,
 }
--- src/base_src/config_example.py
+++ src/base_src/config_example.py
@@ -1,12 +1,12 @@
 # The address to send notifications from
 
-from config_const import *
+from config_const import ranks, keywords
 
 # Credentials for email sending service
 email = {
     "address": "name@gmail.com",
     "password": "password",
-    "port": 465, # this can normally be left at 465
+    "port": 465,  # this can normally be left at 465
 }
 
 # The addresses to send notifications to and their security codes
@@ -21,9 +21,16 @@ mobile_recipients = {
 }
 
 # The information about registered users. The key maps to values in email_/mobile_recipients
-user_info = {1:{"rank": ranks["ADMIN"], "pin": "1234", "salt": "put some salt here"}, 2:{"rank": ranks["USER"], "pin": "5678", "salt": "put different salt here"}}
+user_info = {
+    1: {"rank": ranks["ADMIN"], "pin": "1234", "salt": "put some salt here"},
+    2: {"rank": ranks["USER"], "pin": "5678", "salt": "put different salt here"},
+}
 # Two factor authentication. Set to True if enabled
 two_fact_auth = True
 
 # Notification keywords. Change the keys if you want to provide some security through obscurity
-notif_kw = {"DISABLE": keywords["DISABLE"], "ENABLE": keywords["ENABLE"], "MUTE": keywords["MUTE"]}
+notif_kw = {
+    "DISABLE": keywords["DISABLE"],
+    "ENABLE": keywords["ENABLE"],
+    "MUTE": keywords["MUTE"],
+}
