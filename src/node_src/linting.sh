4a667d54602145e60f11edf7d3c4c9112fa73173
JolonB <jolon.behrent@gmail.com>
2021-05-05 21:42:30 +1200

    Fix code problems

--- src/node_src/linting.sh
+++ src/node_src/linting.sh
@@ -2,7 +2,7 @@
 # files=$(find "main/" "components/" -iregex '.*\.\(cpp\|cu\|hh\|h\|cc\|hpp\|hxx\|h++\|cuh\|cxx\|c++\|c\)$')
 
 printf "cpplint\n"
-cpplint --recursive --filter=-build/include_subdir,-legal/copyright main/ components/
+cpplint --recursive --root=. --filter=-build/include_subdir,-legal/copyright main/ components/
 
 printf "\ncppcheck\n"
-cppcheck --enable=all --inline-suppr --suppress=missingInclude main/ components/
+cppcheck --enable=all --inline-suppr --suppress=missingInclude main/ components/ --error-exitcode=1
