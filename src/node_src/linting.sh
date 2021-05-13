b505730bd656e43700aecc84f21bb8ae1d8a46c1
JolonB <jolon.behrent@gmail.com>
2021-05-13 21:23:30 +1200

    Add unittesting
    Including an example

--- src/node_src/linting.sh
+++ src/node_src/linting.sh
@@ -2,7 +2,7 @@
 # files=$(find "main/" "components/" -iregex '.*\.\(cpp\|cu\|hh\|h\|cc\|hpp\|hxx\|h++\|cuh\|cxx\|c++\|c\)$')
 
 printf "cpplint\n"
-cpplint --recursive --root=. --filter=-build/include_subdir,-legal/copyright main/ components/
+cpplint --recursive --root=. --filter=-build/include_subdir,-legal/copyright --exclude=build/ --exclude=test/build/ --exclude=components/esp32-camera .
 
 printf "\ncppcheck\n"
-cppcheck --enable=all --inline-suppr --suppress=missingInclude main/ components/ --error-exitcode=1
+cppcheck --enable=all --inline-suppr --suppress=missingInclude -i components/esp32-camera/ -i build/ -i test/build --error-exitcode=1 .
