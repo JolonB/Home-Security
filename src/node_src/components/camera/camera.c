8371a70e89549b379e2763a737b68e06f4531464
JolonB <jolon.behrent@gmail.com>
2021-11-08 23:36:23 +1300

    Implement SD card startup

--- src/node_src/components/camera/camera.c
+++ src/node_src/components/camera/camera.c
@@ -8,25 +8,7 @@
 #include "freertos/task.h"
 
 #include "esp_camera.h"
-
-//! update these if they are wrong, but they appear to be correct
-#define CAM_PIN_PWDN 32
-#define CAM_PIN_RESET -1  // software reset will be performed
-#define CAM_PIN_XCLK 0
-#define CAM_PIN_SIOD 26
-#define CAM_PIN_SIOC 27
-
-#define CAM_PIN_D7 35
-#define CAM_PIN_D6 34
-#define CAM_PIN_D5 39
-#define CAM_PIN_D4 36
-#define CAM_PIN_D3 21
-#define CAM_PIN_D2 19
-#define CAM_PIN_D1 18
-#define CAM_PIN_D0 5
-#define CAM_PIN_VSYNC 25
-#define CAM_PIN_HREF 23
-#define CAM_PIN_PCLK 22
+#include "camera.h"
 
 static const char *TAG = "example:take_picture";
 
