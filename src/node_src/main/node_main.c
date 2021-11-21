6a77866f9fd30e18fc0c92d77d6e0d602a8e64ac
JolonB <jolon.behrent@gmail.com>
2021-11-21 16:50:54 +1300

    Add file manager
    For saving images

--- src/node_src/main/node_main.c
+++ src/node_src/main/node_main.c
@@ -14,23 +14,43 @@
 #include "esp_spi_flash.h"
 #include "esp_log.h"
 
-#include "image_saver.h"
+#include "file_manage.h"
 #include "camera.h"
+#include "time.h"
 
-static const char* TAG = "Main";
+static const char* TAG = "main";
 
 void app_main(void) {
-    int i = 0;
-    image_saver_mount_sdcard();
+    uint8_t *image_data = NULL;
+    size_t buffer_len;
+
+    // Init file manager and camera
     camera_init_camera();
-    camera_fb_t* image = camera_get_picture();
-    image_saver_write_image(image);
-    camera_free_fb(image);
-    image_saver_unmount_sdcard();
-    // while (true) {
-    //     printf("Hello world!\n");
-    //     ESP_LOGW(TAG, "This is some logging info with an arg %i", i);
-    //     vTaskDelay(10000 / portTICK_PERIOD_MS);
-    //     i++;
-    // }
+    file_manage_mount_sdcard();
+
+    // Expose the camera
+    camera_expose_camera();
+
+    // Take image
+    int64_t start = esp_timer_get_time();
+    buffer_len = camera_get_picture(&image_data);
+    int64_t end = esp_timer_get_time();
+    ESP_LOGI(TAG, "Time taken to take picture: %d milliseconds", \
+                (int)((end - start)/1000));
+    if (image_data == NULL) {
+        ESP_LOGE(TAG, "Failed to get image data");
+        return;
+    }
+
+    // Save image
+    start = esp_timer_get_time();
+    file_manage_write_image(image_data, buffer_len, "image.jpg");
+    end = esp_timer_get_time();
+    ESP_LOGI(TAG, "Time taken to save image: %d milliseconds", \
+                (int)((end - start)/1000));
+
+    // Clean up
+    free(image_data);
+    file_manage_unmount_sdcard();
+    camera_deinit_camera();
 }
